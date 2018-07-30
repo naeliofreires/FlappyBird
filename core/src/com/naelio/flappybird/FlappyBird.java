e3778c911999bc49a9ee8eec49051690fd340b1d
naeliofreires <naeliosistemas@gmail.com>
2018-07-29 21:07:12 -0300

    adicionado bater das asas mais velocidade de queda

--- core/src/com/naelio/flappybird/FlappyBird.java
+++ core/src/com/naelio/flappybird/FlappyBird.java
@@ -12,35 +12,56 @@ public class FlappyBird extends ApplicationAdapter {
      */
     private SpriteBatch batch;
 
-    private Texture bird;
+    private Texture[] birds;
     private Texture background;
 
-    private int eixo_x = 0;
     private int alturaDispositivo;
     private int larguraDispositivo;
+
+    private int posicaoInicialVertical;
+    private int posicaoInicialHorizontal;
+
+
+    private float variacao = 0; // variável responsável por ficar alternando as imagens dos passaros
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
-        bird = new Texture("passaro1.png"); // recebe uma imagem como parametro
-        background = new Texture("fundo.png");
+        birds = new Texture[3];
+
+        birds[0] = new Texture("passaro1.png");
+        birds[1] = new Texture("passaro2.png");
+        birds[2] = new Texture("passaro3.png");
+
+        background = new Texture("fundo.png"); // recebe uma imagem como parametro
 
         alturaDispositivo = Gdx.graphics.getHeight();
         larguraDispositivo = Gdx.graphics.getWidth();
+
+        posicaoInicialHorizontal = 0;
+        posicaoInicialVertical = alturaDispositivo / 2;
     }
 
     @Override
     public void render() {
 
-        eixo_x++;
+        variacao += Gdx.graphics.getDeltaTime() * 10;
+
+        if (variacao > 2)
+            variacao = 0;
+
+        if(posicaoInicialVertical != 0)
+            posicaoInicialVertical--;
+
         /**
          * Iniciando a exibição das nossas imagens
          */
         batch.begin();
 
         batch.draw(background, 0, 0, larguraDispositivo, alturaDispositivo);
-        batch.draw(bird, eixo_x, 500);
+
+        batch.draw(birds[(int) variacao], posicaoInicialHorizontal, posicaoInicialVertical);
 
         batch.end();
         /**
